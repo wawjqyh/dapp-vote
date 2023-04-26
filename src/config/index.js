1799612595aad016a4260e81c9c90125432750c8
yuhang <1205639813@qq.com>
2023-04-26 14:38:27 +0800

    Revert "test"

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,5 @@ export default {
   contractAddress: '0xaFF59F9bdedC9b34a9F1c2611e44A984E43ff93e',
   env: 'dev', // dev/test
   testProvider: 'ropsten',
-  devProvider: 'http://127.0.0.1:7545',
-  test: 233
+  devProvider: 'http://127.0.0.1:7545'
 };
